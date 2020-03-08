1a8b145d2356b6faa3474f325b5c6148b6248752
Jaime Fajardo <jaimefajardorodas@gmail.com>
2020-03-08 11:33:02 -0500

    First commit local

---
+++ Prj1/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+$("#btnSuma").click(function () {
+	var num1 = $("#in1").val();
+	var num2 = $("#in2").val();
+
+	var resultado = (parseInt(num1) + parseInt(num2) );
+
+	$("#resultado").val(resultado);
+
+});
